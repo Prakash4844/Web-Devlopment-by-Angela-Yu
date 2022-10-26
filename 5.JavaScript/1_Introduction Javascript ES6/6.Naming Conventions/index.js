7507f3678cdc5fe52e84ca7a6b6eeead6ad79e50
Prakash4844 <pk484442@gmail.com>
2022-10-26 20:28:25 +0530

    Added Naming Conventions

---
+++ 5.JavaScript/1_Introduction Javascript ES6/6.Naming Conventions/index.js
@@ -0,0 +1,19 @@
+// camelCase for identifier names (variables and functions).
+
+// All names start with a letter.
+
+var firstName = "John";
+var lastName = "Doe";
+
+var price = 19.90;
+var tax = 0.20;
+
+fullPrice = price + (price * tax);
+
+// Always use the same naming convention for all your code. For example:
+
+// Variable and function names written as camelCase
+// Global variables written in UPPERCASE (We don't, but it's quite common)
+// Constants (like PI) written in UPPERCASE
+
+// Variable can only contain letters, numbers, "$" and "_" but Letters should appear first. 
